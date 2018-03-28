ec412d5249863c0ff274249d4ec175e9846a9a88
Jenelle Basinger <jenellebas@yahoo.com>
2018-03-27 20:58:24 -0500

    added todos and all templates

--- main.py
+++ main.py
@@ -1,54 +1,22 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 import cgi
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-<!doctype html>
-<html>
-    <body>
-        <form action="/hello" method="post">
-            <label for="first-name">First Name</label>
-            <input id="first-name" type="text" name="first_name" />
-            <input type="submit" />
-
-        </form>
-    </body>
-</html>
-"""
-
 @app.route("/")
 def index():
-    return form
+    return render_template('hello_form.html')
 
 @app.route("/hello", methods=['post'])
 def hello():
     first_name = request.form['first_name']
-    return '<h1>Hello, ' + cgi.escape(first_name) + '</h1>'
-
-time_form = """
-    <style>
-        .error {{ color: red}}
-    </style>
-    <h1>Validate Time</h1>
-    <form method='POST'>
-        <label>Hours (24-hour format)
-            <input name ="hours" type="text" value='{hours}' />
-        </label>
-        <p class="error">{hours_error}</p>
-        <label>Minutes
-            <input name ="minutes" type="text" value='{minutes}' />
-        </label>
-        <p class="error">{minutes_error}</p>
-        <input type="submit" value="Validate" />
-    </form>
-    """
+    return render_template('hello_greeting.html', name=first_name)
 
 @app.route('/validate-time')
 def display_time_form():
-    return time_form.format(hours='', hours_error='',
-    minutes='', minutes_error='')
+    return render_template('time_form.html')
 
 def is_integer(num):
     try:
@@ -87,7 +55,7 @@ def validate_time():
         time = str(hours) + ':' + str(minutes)
         return redirect('/valid-time?time={0}'.format(time))
     else:
-        return time_form.format(hours_error=hours_error,
+        return render_template('time_form.html', hours_error=hours_error,
             minutes_error=minutes_error, 
             hours=hours, minutes=minutes)
 
@@ -104,5 +72,15 @@ def print_form_values():
 
     return resp
 
+tasks = []
+
+@app.route('/todos', methods=['post', 'get'])
+def todos():
+
+    if request.method == 'POST':
+        task = request.form['task']
+        tasks.append(task)
+
+    return render_template('todos.html', title="TODOs", tasks=tasks)
 
 app.run()
